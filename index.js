583f40e4e2f258c76659b9656bad5eea043587d5
Joan-Diego Rodriguez <rodriguezjoan@gmail.com>
2015-10-26 18:21:43 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var MixpanelExport = require('mixpanel-data-export');
+
+panel = new MixpanelExport({
+  api_key: process.env.MIXPANEL_API_KEY,
+  api_secret: process.env.MIXPANEL_API_SECRET
+});
+
+var $name = 'rodriguez@iazi.ch';
+
+panel.engage({
+  where: 'properties["$name"] == "' + $name + '"'
+}).then(function(data) {
+  var lastSeen = data.results[0].$properties.$last_seen;
+  console.log($name, 'was last seen on', lastSeen);
+});
+
+module.exports = function(robot) {
+  robot.respond(/mixpanel/i, function(msg){
+    var today = new Date();
+
+    msg.reply('Glad to hear that you want to have some mixpanel stats');
+  });
+};
---
+++ readme.md
@@ -0,0 +1,4 @@
+1) Set your environment variables:
+
+    export MIXPANEL_API_KEY=xxxxx
+    export MIXPANEL_API_SECRET=xxxxx
