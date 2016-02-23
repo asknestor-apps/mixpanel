408bfb7deff6122d9f82b4a8d73c9025ccf9ed39
arunthampi <arun.thampi@gmail.com>
2016-02-22 17:12:00 -0800

    Port to nestor

--- index.js
+++ index.js
@@ -5,18 +5,18 @@
 //   None
 //
 // Configuration:
-//   MIXPANEL_API_KEY     - Your Mixpanel API key
-//   MIXPANEL_API_SECRET  - Your Mixpanel API secret
+//   NESTOR_MIXPANEL_API_KEY     - Your Mixpanel API key
+//   NESTOR_MIXPANEL_API_SECRET  - Your Mixpanel API secret
 //
 // Commands:
-//   hubot mixpanel  - List last 10 users of your app
+//   nestor mixpanel  - List last 10 users of your app
 
 var MixpanelExport = require('mixpanel-data-export');
 var moment = require('moment');
 
-panel = new MixpanelExport({
-  api_key: process.env.MIXPANEL_API_KEY,
-  api_secret: process.env.MIXPANEL_API_SECRET
+var panel = new MixpanelExport({
+  api_key: process.env.NESTOR_MIXPANEL_API_KEY,
+  api_secret: process.env.NESTOR_MIXPANEL_API_SECRET
 });
 
 function compare(a,b) {
@@ -28,28 +28,34 @@ function compare(a,b) {
 }
 
 function getStats() {
-
   return (
-  panel
-    .engage()
-    .then(function(data) {
-      data.results.sort(compare);
-      var message = '*These are your latest users:* \n\r';
-      data.results.slice(0,10).map(function(user){
-        message = message + '  • ' + user.$properties.$name + ': ' + moment().to(user.$properties.$last_seen) + '\n\r';
-      });
-      return message;
-    })
-    .catch(function(err){ console.log(err); })
+    panel
+      .engage()
+      .then(function(data) {
+        message = '';
+        data.results.sort(compare);
+        data.results.slice(0,10).map(function(user){
+          var identifier = user.$properties.$name;
+          if(!identifier) { identifier = user.$properties.full_name; }
+          if(!identifier) { identifier = user.$properties.first_name; }
+          if(!identifier) { identifier = user.$properties.email; }
+
+          message = message + '• ' + identifier + ': ' + moment().to(user.$properties.$last_seen) + '\n\r';
+        });
+        return message;
+      })
+      .catch(function(err){ console.log(err); })
   );
 
 }
 
 module.exports = function(robot) {
-  robot.respond(/mixpanel/i, function(msg){
-    getStats()
-    .then(function(message){
-      msg.reply(message);
+  robot.respond(/mixpanel/i, function(msg, done){
+    msg.reply("Here are your latest users on Mixpanel").then(function() {
+      getStats()
+      .then(function(message){
+        msg.send(message, done);
+      });
     });
   });
 };
